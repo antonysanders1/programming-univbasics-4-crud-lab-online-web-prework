ca2b0b1ed5098b5e3bd6c08adbede13f241ba515
Antony Sanders <antony.devon92@gmail.com>
2019-10-26 18:29:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  Siblings = [Robert, Ashley, Antony, Tiona]
+  [Robert, Ashley, Antony, Tiona]
 end
 
 def add_element_to_end_of_array(array, element)
