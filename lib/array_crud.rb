bfa1076bbdaf107ec717773db59e413f7ca9ffdd
Antony Sanders <antony.devon92@gmail.com>
2019-10-26 18:48:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,15 +12,16 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+  array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  siblings.pop
+  
 end
 
 def remove_element_from_start_of_array(array)
-  siblings.shift
+  
 end
 
 def retrieve_element_from_index(array, index_number)
